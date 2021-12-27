56201ba84d9dc1b5df4c6f3caf6a99d05f4e333c
Suryash-Jha <joblesscoder2468@gmail.com>
2021-12-27 22:32:22 +0530

    Queue at the School solution

---
+++ Queue_at_the_School.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fo(i, n) for(int i=0;i<n;i++)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mod 1e9 +7
+#define w(x) int x; cin>>x; while(x--)
+#define endl "\n"
+#define vp(i, a, n) vi a; fo(i, n){int x; cin>>x; a.pb(x);}
+#define ap(i, a, n) fo(i, n){cin>>a[i];}
+
+typedef vector<int> vi;
+
+
+void solution() {
+   int n, k;
+   cin>>n>>k;
+   string str;
+   cin>>str;
+   while(k--)
+   	{
+   		fo(i, n)
+   		{
+   			if(str[i]=='B' && str[i+1]=='G')
+   			{
+   				swap(str[i], str[i+1]);
+   				i++;
+   			}
+   		}
+
+   	}
+   	cout<<str;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    // w(t)
+    {
+        solution();
+    }
+   
+    return 0;
+}
