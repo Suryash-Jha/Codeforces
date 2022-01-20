d789695c4dfa06a06a6971893c51311bb435174a
Suryash-Jha <joblesscoder2468@gmail.com>
2022-01-20 10:30:45 +0530

    New Year Candles *1000 O(logn)

---
+++ New_Year_Candles.cpp
@@ -0,0 +1,51 @@
+//Question Link: https://codeforces.com/contest/379/problem/A
+
+
+//O(logn)
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+#define fo(i, n) for(int i=0;i<n;i++)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mod 1e9 +7
+#define w(x) int x; cin>>x; while(x--)
+#define endl "\n"
+#define vp(i, a, n) vi a; fo(i, n){int x; cin>>x; a.pb(x);}
+#define ap(i, a, n) fo(i, n){cin>>a[i];}
+
+typedef vector<int> vi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //*find_by_order(val);   order_of_key(val);
+
+void solution() {
+ int n, k, ans=0, carry;
+   cin>>n>>k;
+   ans+= n;
+   while(n>=k)
+   {
+    int div= n/k;
+    ans+= div;
+    int rem = n%k;
+    n= div+ rem;
+   }
+   // cout<<ans<<endl;
+ 
+   cout<<ans<<endl;
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    // w(t)
+    {
+        solution();
+    }
+   
+    return 0;
+}
