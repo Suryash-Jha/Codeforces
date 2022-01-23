3e7117f3d10bf87617a8d99df33df9afcffebe91
Suryash-Jha <joblesscoder2468@gmail.com>
2022-01-23 14:44:47 +0530

    Arpa hard exam solution *1000

---
+++ Arpa’s_hard_exam_and_Mehrdad’s_naive_cheat.cpp
@@ -0,0 +1,53 @@
+//Question Link: https://codeforces.com/contest/742/problem/A
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+#define fo(i, n) for(int i=0;i<n;i++)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mod 1e9 +7
+#define w(x) int x; cin>>x; while(x--)
+#define endl "\n"
+#define vp(i, a, n) vi a; fo(i, n){int x; cin>>x; a.pb(x);}
+#define ap(i, a, n) fo(i, n){cin>>a[i];}
+
+typedef vector<int> vi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //*find_by_order(val);   order_of_key(val);
+
+void solution() {
+   int n;
+   cin>>n;
+   if(n==0){
+    cout<<1<<endl;
+    return;
+   }
+   if(n%4==1){
+    cout<<8<<endl;
+   }
+   else if(n%4==2){
+    cout<<4<<endl;
+   }
+   else if(n%4==3){
+    cout<<2<<endl;
+   }
+   else if(n%4==0){
+    cout<<6<<endl;
+   }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    // w(t)
+    {
+        solution();
+    }
+   
+    return 0;
+}
