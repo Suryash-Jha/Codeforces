3db940a6e377fd4eb45ad1bf58d90e29eee3c305
Suryash-Jha <joblesscoder2468@gmail.com>
2022-01-27 19:29:13 +0530

    Deadline *1100

---
+++ Deadline.cpp
@@ -0,0 +1,48 @@
+//Question Link: https://codeforces.com/contest/1288/problem/A
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+#define fo(i, n) for(int i=0;i<n;i++)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mod 1e9 +7
+#define w(x) int x; cin>>x; while(x--)
+#define endl "\n"
+#define vp(i, a, n) vi a; fo(i, n){int x; cin>>x; a.pb(x);}
+#define ap(i, a, n) fo(i, n){cin>>a[i];}
+
+typedef vector<int> vi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //*find_by_order(val);   order_of_key(val);
+
+void solution() {
+   int n, d;
+   cin>>n>>d;
+int ans= INT_MAX;
+  fo(x, 300000){
+    int val= x+ (d+x)/(x+1);
+     ans= min(ans, val);
+  }
+  if(ans<=n){
+    cout<<"YES"<<endl;
+  }
+  else{
+    cout<<"NO"<<endl;
+  }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    w(t)
+    {
+        solution();
+    }
+   
+    return 0;
+}
