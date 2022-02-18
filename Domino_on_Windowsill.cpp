bf23559f2e511bc0ad0e218a60bb77697eea92a4
Suryash-Jha <joblesscoder2468@gmail.com>
2022-02-18 20:51:34 +0530

    Domino on Windowsill easy

--- Domino_on_Windowsill.cpp
+++ Domino_on_Windowsill.cpp
@@ -1,5 +1,7 @@
 //Question Link: https://codeforces.com/problemset/problem/1499/A
 
+//Easy One!!!
+
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -23,76 +25,26 @@ typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_
 
 void solution() {
    int n, k1, k2;
-   cin>>n>>k1>>k2;
    int w, b;
-   cin>>w>>b;
-   int arr[2][n];
-   fo(i, 2){
-    fo(j, n){
-      arr[i][j]=0;
-    }
+   cin>>n>>k1>>k2>>w>>b;
+   int cw=0, cb=0;
+   cw+= min(k1, k2);
+   cb+= min(n-k1, n-k2);
+   cw+= abs(k2-k1)/2;
+   cb+= abs((n-k1) - (n-k2))/2;
+   if(cw>= w && cb>=b){
+    cout<<"YES"<<endl;
    }
-fo(i, k1){
-  arr[0][i]= 1;
-}
-fo(i, k2){
-  arr[1][i]= 1;
-}
-// fo(i, 2){
-//   fo(j, n){
-//     cout<<arr2[i][j]<<" ";
-//   }
-//   cout<<endl;
-// }
-int cw=0, cb=0;
-fo(i, n){
-  if(arr[0][i]== 1 && arr[1][i]== 1){
-    cw++;
-  }
-  else if(arr[0][i]==0 && arr[1][i]==0){
-    cb++;
-  }
-  else{
-    if(arr[0][i]== 1 && arr[1][i]== 0){
-      if(arr[0][i+1]==1 && arr[1][i+1]==0){
-        cw++;
-        cb++;
-        i++;
-      }
-    }
-    else if(arr[0][i]== 0 && arr[1][i]== 1){
-      if(arr[0][i+1]==0 && arr[1][i+1]==1){
-        cw++;
-        cb++;
-        i++;
-      }
-    }
-  }
-}
-
-// cout<<cw<<" "<<cb<<endl;
-if(cw< w || cb< b){
-  cout<<"NO"<<endl;
-}
-else{
-  cout<<"YES"<<endl;
-}
-   // int answ=0;
-   // int ansb=0;
-   // answ+= min(k1, k2);
-   // ansb+= min(n-k1, n-k2);
-
-   // cout<<answ<<" "<<ansb<<endl;
-
+   else{
+    cout<<"NO"<<endl;
+   }
+   // cout<<cw<<" "<<cb<<endl;
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int count=0;
     w(t)
     {
-    // count++;
-    // cout<<"#"<<count<<endl;
         solution();
     }
    
