0bc099bbabea2a370c0e26cb35ae44ca19d7998a
Suryash-Jha <joblesscoder2468@gmail.com>
2022-02-27 21:59:53 +0530

    Towers *1000

---
+++ Towers.cpp
@@ -0,0 +1,51 @@
+//Question link: https://codeforces.com/problemset/problem/37/A
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+#define fo(i, n) for(int i=0;i<n;i++)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mod 1e9 +7
+#define w(x) int x; cin>>x; while(x--)
+#define endl "\n"
+#define vp(i, a, n) vi a; fo(i, n){int x; cin>>x; a.pb(x);}
+#define ap(i, a, n) fo(i, n){cin>>a[i];}
+
+typedef vector<int> vi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //*find_by_order(val);   order_of_key(val);
+
+void solution() {
+   int n;
+   cin>>n;
+   vp(i, v, n);
+   int arr[1001]={0};
+   // sort(v.begin(), v.end());
+   fo(i, n){
+      arr[v[i]]++;
+   }
+   
+   cout<<*max_element(arr, arr+1001)<<" ";
+   int count=0;
+   fo(i, 1001){
+    if(arr[i]!=0) count++;
+   }
+   cout<<count<<endl;
+   
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    // w(t)
+    {
+        solution();
+    }
+   
+    return 0;
+}
