ff7015600785f91ee2950e805a1f1efeb7aaec5a
Suryash-Jha <joblesscoder2468@gmail.com>
2022-03-05 11:43:54 +0530

    Random_Teams.cpp

---
+++ Random_Teams.cpp
@@ -0,0 +1,52 @@
+//Question link: https://codeforces.com/contest/478/problem/B
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+#define fo(i, n) for(int i=0;i<n;i++)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mod 1e9 +7
+#define w(x) int x; cin>>x; while(x--)
+#define endl "\n"
+#define vp(i, a, n) vi a; fo(i, n){int x; cin>>x; a.pb(x);}
+#define ap(i, a, n) fo(i, n){cin>>a[i];}
+
+typedef vector<int> vi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //*find_by_order(val);   order_of_key(val);
+
+void solution() {
+   
+int n, m;
+cin>>n>>m;
+int minm=0, maxm;
+maxm= (n-m)*(n-m+1)/2;
+int q=n/m;
+int r= n%m;
+
+minm= q*(q-1)*(m-r)/2;
+
+minm+= r*q*(q+1)/2;
+// cout<<minm<<endl;
+cout<<minm<<" "<<maxm<<endl;
+
+
+}
+
+
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    // w(t)
+    {
+        solution();
+    }
+   
+    return 0;
+}
