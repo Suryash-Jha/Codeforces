cd7996ba30f59829eeb7dc4e0a9c1b7349b7f030
Suryash-Jha <joblesscoder2468@gmail.com>
2022-03-22 00:16:24 +0530

    Wizard_of_orz.cpp *1000

---
+++ Wizard_of_orz.cpp
@@ -0,0 +1,55 @@
+//Question Link: https://codeforces.com/problemset/problem/1467/A
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+#define fo(i, n) for(int i=0;i<n;i++)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mod 1e9 +7
+#define w(x) int x; cin>>x; while(x--)
+#define endl "\n"
+#define vp(i, a, n) vi a; fo(i, n){int x; cin>>x; a.pb(x);}
+#define ap(i, a, n) fo(i, n){cin>>a[i];}
+
+typedef vector<int> vi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //*find_by_order(val);   order_of_key(val);
+
+void solution() {
+  int n;
+  cin>>n;
+  if(n==1){
+    cout<<9<<endl;
+  } 
+  else if(n==2){
+    cout<<98<<endl;
+  } 
+  else if(n==3){
+    cout<<989<<endl;
+  } 
+  else{
+    cout<<989;
+    for(int i=0; i<n-3; i++){
+
+      cout<<i%10;
+    }
+    cout<<endl;
+  }
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    w(t)
+    {
+        solution();
+    }
+   
+    return 0;
+}
