780f8d740aca52e839a720da752b6949a00f3ca0
Suryash-Jha <joblesscoder2468@gmail.com>
2022-03-31 10:57:25 +0530

    Single_Push.cpp

---
+++ Single_Push.cpp
@@ -0,0 +1,77 @@
+//Question lInk: https://codeforces.com/problemset/problem/1253/A
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+#define fo(i, n) for(int i=0;i<n;i++)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mod 1e9 +7
+#define w(x) int x; cin>>x; while(x--)
+#define endl "\n"
+#define vp(i, a, n) vi a; fo(i, n){int x; cin>>x; a.pb(x);}
+#define ap(i, a, n) fo(i, n){cin>>a[i];}
+
+typedef vector<int> vi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //*find_by_order(val);   order_of_key(val);
+
+void solution() {
+   int n;
+   cin>>n;
+   vp(i, v, n);
+   vp(i, v1, n);
+   int ind=0;
+   int ind1=0;
+   fo(i, n){
+    if(v[i]!= v1[i]) 
+      {ind= i;
+    break;
+  }
+   }
+      for(int i=n-1; i>=0; i--){
+    if(v[i]!= v1[i]) 
+      {ind1= i;
+    break;
+  }
+   }
+   vi ans;
+   bool b=0;
+  for(int i=ind; i<=ind1; i++){
+    ans.pb(v1[i]- v[i]);
+  }
+
+  fo(i, ans.size()-1){
+    if(ans[i]!= ans[i+1]){
+    b=1;
+    break;
+    }
+  }
+    fo(i, ans.size()){
+    if(ans[i]<0){
+    b=1;
+    break;
+    }
+  }
+   if(b){
+    cout<<"NO"<<endl;
+   }
+   else{
+    cout<<"YES"<<endl;
+   }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    w(t)
+    {
+        solution();
+    }
+   
+    return 0;
+}
